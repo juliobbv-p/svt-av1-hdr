bbaabf4390081aedddc971051b2b83666066ae02
Julio Barba <juliobbv@gmail.com>
2024-03-27 00:08:22 -0400

    Implement QP scale compress

--- Source/API/EbSvtAv1Enc.h
+++ Source/API/EbSvtAv1Enc.h
@@ -936,6 +936,14 @@ typedef struct EbSvtAv1EncConfiguration {
      */
     uint8_t extended_crf_qindex_offset;
 
+    /* @brief compresses the QP hierarchical layer scale to improve temporal video consistency
+     * 0: no compression, original SVT-AV1 scaling
+     * 1-3: enable compression, the higher the number the stronger the compression
+     *      (different frame quality fluctuation/mean quality tradeoffs)
+     * Default is 1
+     */
+    uint8_t qp_scale_compress_strength;
+
     /**
      * @brief Min chroma quant matrix flatness. Applicable when enable_qm is true.
      * Min value is 0.
