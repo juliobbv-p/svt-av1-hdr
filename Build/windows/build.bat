gianni-rosato/svt-av1-psy

fix: Add enable libdovi flag windows build.bat

Build/windows/build.bat
@@ -158,6 +158,9 @@ if -%1-==-- (
 ) else if /I "%1"=="avx512" (
     set "cmake_eflags=%cmake_eflags% -DENABLE_AVX512=ON"
     shift
+) else if /I "%1"=="enable-libdovi" (
+    set "cmake_eflags=%cmake_eflags% -DLIBDOVI_FOUND=1"
+    shift
 ) else if /I "%1"=="lto" (
     set "cmake_eflags=%cmake_eflags% -DSVT_AV1_LTO=ON"
     shift
@@ -179,6 +182,6 @@ goto :args
 
 :help
     echo Batch file to build SVT-AV1 on Windows
-    echo Usage: build.bat [2022^|2019^|2017^|2015^|clean] [release^|debug] [nobuild] [test] [shared^|static] [c-only] [avx512] [no-apps] [no-dec] [no-enc]
+    echo Usage: build.bat [2022^|2019^|2017^|2015^|clean] [release^|debug] [nobuild] [test] [shared^|static] [c-only] [avx512] [enable-libdovi] [no-apps] [no-dec] [no-enc]
     exit /b 1
 goto :EOF

juliobbv-p/svt-av1-hdr

Build/windows/build.bat
@@ -158,6 +158,9 @@ if -%1-==-- (
 ) else if /I "%1"=="avx512" (
     set "cmake_eflags=%cmake_eflags% -DENABLE_AVX512=ON"
     shift
+) else if /I "%1"=="enable-libdovi" (
+    set "cmake_eflags=%cmake_eflags% -DLIBDOVI_FOUND=1"
+    shift
 ) else if /I "%1"=="lto" (
     set "cmake_eflags=%cmake_eflags% -DSVT_AV1_LTO=ON"
     shift
@@ -179,6 +182,6 @@ goto :args
 
 :help
     echo Batch file to build SVT-AV1 on Windows
-    echo Usage: build.bat [2022^|2019^|2017^|2015^|clean] [release^|debug] [nobuild] [test] [shared^|static] [c-only] [avx512] [no-apps] [no-enc] [external-cpuinfo]
+    echo Usage: build.bat [2022^|2019^|2017^|2015^|clean] [release^|debug] [nobuild] [test] [shared^|static] [c-only] [avx512] [enable-libdovi] [no-apps] [no-enc] [external-cpuinfo]
     exit /b 1
 goto :EOF