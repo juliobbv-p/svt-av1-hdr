cd0e0a4572f4e87ac9044b0e317ff2dda0f85e6d
BlueSwordM <neutronpcxt@gmail.com>
2025-04-06 22:30:24 -0400

    fix 1: removal of default tune 3 noise norm strength; default now 1

--- Source/Lib/Codec/full_loop.c
+++ Source/Lib/Codec/full_loop.c
@@ -1488,11 +1488,6 @@ void svt_av1_perform_noise_normalization(MacroblockPlane *p,
 
     uint8_t noisenorm_strength = pcs->scs->static_config.noise_norm_strength;
 
-    // If tune is 3 & noisenorm_strength is 0, set noisenorm_strength to 3
-    if (noisenorm_strength < 1 && tune == 3) {
-        noisenorm_strength = 3;
-    }
-
     // If noisenorm_strength is 0, terminate early
     if (noisenorm_strength < 1) {
         return;
