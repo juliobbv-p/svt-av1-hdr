7a6cb36538bf05f0aa5b5acb488b8ee5494d0008
Julio Barba <juliobbv@gmail.com>
2025-11-22 17:24:25 -0500

    Rebrand to SVT-AV1-HDR

--- Source/App/app_config.c
+++ Source/App/app_config.c
@@ -1734,7 +1734,16 @@ int get_version(int argc, char *argv[]) {
 #endif
     if (find_token(argc, argv, VERSION_TOKEN, NULL))
         return 0;
-    printf("SVT-AV1 %s (%s)\n", svt_av1_get_version(), debug_build ? "release" : "debug");
+    printf("SVT-AV1-HDR %s (%s)\n", svt_av1_get_version(), debug_build ? "release" : "debug");
+#if defined(_WIN64) || defined(_MSC_VER) || defined(_WIN32)
+    printf("HDR Release: %s\n", svt_hdr_get_version());
+#else
+    if (strcmp(svt_hdr_get_version(), "N/A")) {
+        printf("HDR Release: \x1b[32m%s\x1b[0m\n", svt_hdr_get_version());
+    } else {
+        printf("HDR Release: \x1b[38;5;248m%s\x1b[0m\n", svt_hdr_get_version());
+    }
+#endif
     return 1;
 }
 
