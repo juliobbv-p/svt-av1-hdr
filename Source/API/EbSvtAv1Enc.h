904fdeee7b4953b28b4d0c8be0a3dd10a7a61d8c
Yiss <62639703+Uranite@users.noreply.github.com>
2025-12-15 11:50:54 +0700

    Noise adaptive filtering (#15)

--- Source/API/EbSvtAv1Enc.h
+++ Source/API/EbSvtAv1Enc.h
@@ -1036,6 +1036,17 @@ typedef struct EbSvtAv1EncConfiguration {
      */
      uint8_t complex_hvs;
 
+     /**
+     * @brief Controls noise detection for CDEF/restoration filtering
+     * 0: off
+     * 1: always-on noise-adaptive filters
+     * 2: default tune behavior
+     * 3: noise-adaptive CDEF only
+     * 4: noise-adaptive restoration filtering only
+     * Default is 2
+     */
+     uint8_t noise_adaptive_filtering;
+
     /*Add 128 Byte Padding to Struct to avoid changing the size of the public configuration struct*/
     uint8_t padding[128
     ];
